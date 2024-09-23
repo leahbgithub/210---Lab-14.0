9547deca71f73e9cf18bdc02b92802966d5a0e71
leahbgithub <leahchaku@gmail.com>
2024-09-22 18:22:11 -0700

    updated again!!!!

--- 210 - Lab 14.0/main.cpp
+++ 210 - Lab 14.0/main.cpp
@@ -9,8 +9,10 @@ private:
     int blue;
 
 public:
+    // Constructor for Color
     Color(int r = 0, int g = 0, int b = 0) : red(r), green(g), blue(b) {}
     
+    // Setters
     void setRed(int r) {
         red = (r >= 0 && r <= 255) ? r : 0;
     }
@@ -23,6 +25,7 @@ public:
         blue = (b >= 0 && b <= 255) ? b : 0;
     }
     
+    // Getters
     int getRed() const {
         return red;
     }
@@ -34,4 +37,18 @@ public:
     int getBlue() const {
         return blue;
     }
+    
+    // Print statement which handles outputting the RGB conversions of colors
+    void print() const {
+        cout << "RGB values of Color - " << red << ", " << green << ", " << blue << endl;
+    }
 };
+
+int main() {
+    // Now we create objects of Color
+    Color num1;
+    Color num2(255, 0, 0); // For red
+    Color num3(0, 255, 0); // For green
+    Color num4(0, 0, 255); // For blue
+    
+
